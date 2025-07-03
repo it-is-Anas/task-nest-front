111239f8cd82868bb9354ee3d4ee8568c46bd5a3
prontoTheLegend <anasprog8@gmail.com>
2025-07-03 10:08:37 +0300

    Finished project page work space

---
+++ src/components/Headers/ProjectHeader.tsx
@@ -0,0 +1,8 @@
+export default function ProjectHeader(){
+    return (
+        <header className="project-page_header" >
+            <p className="inbox-header_logo">Projects</p>
+        </header>
+        
+    );
+}
--- src/pages/WorkSpacePages/Projects.tsx
+++ src/pages/WorkSpacePages/Projects.tsx
@@ -1,6 +1,49 @@
+import ProjectHeader from "../../components/Headers/ProjectHeader";
+import ProjectCart from "../../components/Projects/ProjectCard";
 
 export default function UserProjects(){
     return (
-        <div className="work-space-page flex-center">user Projects</div>
+        <div className="work-space-page project-page">
+            <ProjectHeader />
+            <div className="project-page_body">
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+                <ProjectCart></ProjectCart>
+            </div>
+        </div>
     );
-}
+} 
